16689f964f2259caba90841e4ceabfe97093c7fe
Nicolás Miari <nicolasmiari@gmail.com>
2020-05-04 13:41:36 +0900

    Fix interface of RadioButton - RadioGroup

--- Sources/SpriteKitControls/RadioButton.swift
+++ Sources/SpriteKitControls/RadioButton.swift
@@ -9,12 +9,26 @@ import SpriteKit
 
 open class RadioButton: Control {
 
-    private(set) weak var group: RadioGroup?
+    private(set) var group: RadioGroup?
 
     // MARK: - Operation
 
+    /**
+     Buttons retain the group they belong to. The group only has weak
+     references. This way, you don't need to keep a reference to the group (and
+     the buttons themselves are kept alive by the node hierarchy).
+     */
     public func addToGroup(_ group: RadioGroup) {
+        if self.group != nil {
+            removeFromGroup()
+        }
         group.addButton(self)
+        self.group = group
+    }
+
+    public func removeFromGroup() {
+        group?.removeButton(self)
+        self.group = nil
     }
 
     // MARK: - Intents
@@ -38,27 +52,49 @@ open class RadioButton: Control {
 
 public class RadioGroup {
 
-    private(set) var buttons: [RadioButton]
+    private var buttonReferences: [WeakReference<RadioButton>]
 
     public init() {
-        self.buttons = []
+        self.buttonReferences = []
     }
 
     public func addButton(_ button: RadioButton) {
-        if buttons.contains(button) {
+        if buttonReferences.contains(where: { (weakReference) -> Bool in
+            return weakReference.object === button
+        }) {
             return
         }
-        buttons.append(button)
+        buttonReferences.append(WeakReference(button))
+    }
+
+    public func removeButton(_ button: RadioButton) {
+        guard let index = buttonReferences.firstIndex (where: {
+            $0.object === button
+        }) else {
+            return
+        }
+        buttonReferences.remove(at: index)
     }
 
     public func select(_ selectedButton: RadioButton) {
-        buttons.forEach { (button) in
-            if button == selectedButton {
+        buttonReferences.forEach { (reference) in
+            guard let button = reference.object else {
+                return
+            }
+            if button === selectedButton {
                 button.state = .selected
             } else {
                 button.state = .normal
             }
         }
+    }
+}
+
+private struct WeakReference<Type: AnyObject> {
+    weak var object: Type?
+
+    init(_ object: Type) {
+        self.object = object
     }
 }
 
